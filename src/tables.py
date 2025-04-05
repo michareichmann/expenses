7972e2d0d83b01eb816753836ef09ac946f08e40
michareichmann <micha.reichmann@gmail.com>
2025-04-05 23:31:23 +0200

    define exclude tables

--- src/tables.py
+++ src/tables.py
@@ -5,20 +5,20 @@ from sqlalchemy.orm import declarative_base, relationship
 Base = declarative_base()
 
 
-class ExcludeMeta(Base):
+class TExcludeMeta(Base):
     __tablename__ = 'exclude_meta'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     column_name = Column(String, nullable=False, unique=True)
 
-    exclude = relationship('Exclude', back_populates='exclude_meta', cascade='all, delete')
+    exclude = relationship('TExclude', back_populates='exclude_meta', cascade='all, delete')
 
 
-class Exclude(Base):
+class TExclude(Base):
     __tablename__ = 'exclude'
 
     tags = Column(String, primary_key=True)
     exclude_id = Column(Integer, ForeignKey('exclude_meta.id'), nullable=False)
 
-    exclude_meta = relationship('ExcludeMeta', back_populates='exclude')
+    exclude_meta = relationship('TExcludeMeta', back_populates='exclude')
 
