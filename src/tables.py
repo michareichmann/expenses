a53ad0543d092ce8cf7c0eea69ca2c29cc050f69
michareichmann <micha.reichmann@gmail.com>
2025-04-20 10:27:03 +0200

    add TCategory and unity meta table

--- src/tables.py
+++ src/tables.py
@@ -5,20 +5,30 @@ from sqlalchemy.orm import declarative_base, relationship
 Base = declarative_base()
 
 
-class TExcludeMeta(Base):
-    __tablename__ = 'exclude_meta'
+class TMeta(Base):
+    __tablename__ = 'meta'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    column_name = Column(String, nullable=False, unique=True)
+    tag_type = Column(String, nullable=False, unique=True)
 
-    exclude = relationship('TExclude', back_populates='exclude_meta', cascade='all, delete')
+    exclude = relationship('TExclude', back_populates='meta', cascade='all, delete')
+    category = relationship('TCategory', back_populates='meta', cascade='all, delete')
 
 
 class TExclude(Base):
     __tablename__ = 'exclude'
 
     tags = Column(String, primary_key=True)
-    exclude_id = Column(Integer, ForeignKey('exclude_meta.id'), nullable=False)
+    meta_id = Column(Integer, ForeignKey('meta.id'), nullable=False)
 
-    exclude_meta = relationship('TExcludeMeta', back_populates='exclude')
+    meta = relationship('TMeta', back_populates='exclude')
 
+
+class TCategory(Base):
+    __tablename__ = 'category'
+
+    category = Column(String, primary_key=True)
+    tags = Column(String, primary_key=True)
+    meta_id = Column(Integer, ForeignKey('meta.id'), nullable=False)
+
+    meta = relationship('TMeta', back_populates='category')
